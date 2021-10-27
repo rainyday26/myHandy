669fc3873e0cb22fe23006224f03d19c6272f46b
jabiim <jabiim00@gmail.com>
2021-10-27 21:26:30 +0200

    fix: window style

--- script.js
+++ script.js
@@ -244,28 +244,16 @@ class Hander {
 		const head = document.createElement("div");
 		
 		const window = document.createElement("div");
-		window.style =  "position: absolute; " + 
-	  window.style =  "position: absolute; " + 
-		window.style =  "position: absolute; " + 
-		"top: 0px; " + 
-					  "top: 0px; " + 
-		"top: 0px; " + 
-		"left: 0px; " + 
-					  "left: 0px; " + 
-		"left: 0px; " + 
-		"z-index: 2147483647; " + 
-					  "z-index: 2147483647; " + 
-		"z-index: 2147483647; " + 
-		"background-color: #f1f1f1; " + 
-					  "background-color: #f1f1f1; " + 
-		"background-color: #f1f1f1; " + 
-		"border: 1px solid #d3d3d3; " + 
-					  "border: 1px solid #d3d3d3; " + 
-		"border: 1px solid #d3d3d3; " + 
-		"color: #000000; " + 
-					  "color: #000000; " + 
-		"color: #000000; " + 
-		"text-align: center; ";
+		window.style = `
+			position: absolute; 
+			top: 0px; 
+			left: 0px; 
+			z-index: 2147483647; 
+			background-color: #f1f1f1; 
+			border: 1px solid #d3d3d3; 
+			color: #000000; 
+			text-align: center; 
+		`;
 		window.id = "nodudewashere";
 		head.appendChild(window);
 		
