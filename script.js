2e4162b28674f36cd15206574964498b2ec25aa9
rainyday26 <163602448+rainyday26@users.noreply.github.com>
2024-03-15 17:26:45 -0500

    script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 // @name		 theHandy support for PornHub
 // @namespace	http://tampermonkey.net/
 // @version	  2.1.1
-// @downloadURL https://raw.githubusercontent.com/NodudeWasTaken/theHandy_Web/master/script.js
-// @updateURL https://raw.githubusercontent.com/NodudeWasTaken/theHandy_Web/master/script.js
+// @downloadURL https://raw.githubusercontent.com/rainyday26/myHandy/master/script.js
+// @updateURL https://raw.githubusercontent.com/rainyday26/myHandy/master/script.js
 // @description  Web support for the Handy
 // @author	   Nodude
 // @match		*://*/*
